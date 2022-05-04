cc7a14682a01839c4b948912bf43f2e0da09f7cf
mattwg <mattwglowacki@gmail.com>
2022-05-04 17:22:02 -0400

    Created user input function

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -3,24 +3,46 @@ let computerWins = 0;
 
 // Initialize variable for player's wins
 let playerWins = 0;
+
 // Initialize variable for rounds played
 let round = 1;
 console.log(round);
+
 // Computer will choose it's play
-function computerSelection() {
+let computerSelection = computerPlay();
+
+// Human will choose their play
+let playerSelection = userPlay();
+
+console.log(playerSelection);
+function computerPlay() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
     const num = Math.floor(Math.random() * 3);
-    console.log(num);
-    let choices = ['Rock', 'Paper', 'Scissors'];
     
+    let computerChoice = choices[num];    
+    console.log("Computer's Choice:" + computerChoice);
+
     return choices[num];
 }
+
 // Human will choose their play
-function playerSelection() {
-
-
-    // Player will be prompted to input choice
-    // Player input will be made case insensitive
-    // Player input will be checked against acceptable inputs
+function userPlay() {
+    const userChoices = ['Rock', 'Paper', 'Scissors']
+    let input = '';
+    let pass = false;
+    while(pass === false) {
+            // Player will be prompted to input choice
+        input = prompt("Please enter Rock, Paper, or Scissors");
+            // Player input will be made case insensitive
+        input = input[0].toUpperCase() + input.slice(1).toLowerCase();
+            // Player input will be checked against acceptable inputs
+        if (userChoices.includes(input)) {
+            pass = true;
+        } else {
+            console.log('Please enter a valid input.')
+        }
+    }
+    return input;
 }
     // Player and computer plays will be compared
 // Win counter will be incremented by 1 for the player that wins that round
