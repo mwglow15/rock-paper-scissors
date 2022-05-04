167d0906251f175909709e8023ee3f04797690d8
mattwg <mattwglowacki@gmail.com>
2022-05-04 12:50:57 -0400

    Create RockPaperScissors.js and write initial pseudocode

---
+++ RockPaperScissors.js
@@ -0,0 +1,12 @@
+// Initialize variable for computer's wins
+// Initialize variable for player's wins
+// Initialize variable for rounds played
+// Computer will choose it's play
+// Human will choose their play
+    // Player will be prompted to input choice
+    // Player input will be made case insensitive
+    // Player input will be checked against acceptable inputs
+// Player and computer plays will be compared
+// Win counter will be incremented by 1 for the player that wins that round
+// When 5 rounds have been played, the game will end
+// The player with more wins will be declared the winner
