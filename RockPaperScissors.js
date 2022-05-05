22d03a70947b78e7606e19e02aecbef2a2743fa3
mattwg <mattwglowacki@gmail.com>
2022-05-04 21:32:41 -0400

    Complete game logic

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -6,21 +6,40 @@ let playerWins = 0;
 
 // Initialize variable for rounds played
 let round = 1;
-console.log(round);
 
-// Computer will choose it's play
-let computerSelection = computerPlay();
+game();
 
-// Human will choose their play
-let playerSelection = userPlay();
+function game() {
+    while(round <= 5) {
+        console.log("Round " + round);
+        // Computer will choose it's play
+        let computerSelection = computerPlay();
 
-console.log(playerSelection);
+        // Human will choose their play
+        let playerSelection = userPlay();
+
+        console.log("Player's Choice: " + playerSelection);
+
+            // Player and computer plays will be compared
+        console.log(playRound(playerSelection, computerSelection));
+        // Win counter will be incremented by 1 for the player that wins that round
+        console.log("Player wins: " + playerWins + " -- Computer Wins: " + computerWins);
+        // When 5 rounds have been played, the game will end
+    }
+        // The player with more wins will be declared the winner
+    if (playerWins > computerWins) {
+        console.log("You win!!!");
+    } else {
+        console.log("You lose :(");
+    }
+}
+
 function computerPlay() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     const num = Math.floor(Math.random() * 3);
     
     let computerChoice = choices[num];    
-    console.log("Computer's Choice:" + computerChoice);
+    console.log("Computer's Choice: " + computerChoice);
 
     return choices[num];
 }
@@ -44,7 +63,41 @@ function userPlay() {
     }
     return input;
 }
-    // Player and computer plays will be compared
-// Win counter will be incremented by 1 for the player that wins that round
-// When 5 rounds have been played, the game will end
-// The player with more wins will be declared the winner
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === playerSelection) {
+        return "Tie! Play again!";
+    } else if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            round++;
+            computerWins++;
+            return "You lose! Paper covers Rock.";
+        }
+        else if (computerSelection === "Scissors") {
+            round++;
+            playerWins++;
+            return "You win!. Rock crushes Scissors.";
+        }
+    } else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            round++;
+            playerWins++;
+            return "You win! Paper covers Rock.";
+        }
+        else if (computerSelection === "Scissors") {
+            round++;
+            computerWins++;
+            "You lose! Scissors cut Paper.";
+        }
+    } else {
+        if (computerSelection === "Rock") {
+            round++;
+            computerWins++;
+            return "You lose! Rock crushes Scissors.";
+        } else if (computerSelection === "Paper") {
+            round++;
+            playerWins++;
+            return "You win! Scissors cut Paper.";
+        }
+    }
+}
