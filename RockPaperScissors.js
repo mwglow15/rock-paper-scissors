d0766cc68429631d78ff4b874941cc1169a09245
mattwg <mattwglowacki@gmail.com>
2022-05-16 20:42:15 -0400

    Refactor to use eventListener inputs

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -7,25 +7,23 @@ let playerWins = 0;
 // Initialize variable for rounds played
 let round = 1;
 
-game();
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => button.addEventListener('click',
+    playRound
+));
+
+const roundContent = document.querySelector(".round");
+const score = document.querySelector(".score");
+const callout = document.querySelector(".callout");
+
+// game();
 
 function game() {
-    while(round <= 5) {
-        console.log("Round " + round);
-        // Computer will choose it's play
-        let computerSelection = computerPlay();
 
-        // Human will choose their play
-        let playerSelection = userPlay();
-
-        console.log("Player's Choice: " + playerSelection);
-
-            // Player and computer plays will be compared
-        console.log(playRound(playerSelection, computerSelection));
-        // Win counter will be incremented by 1 for the player that wins that round
-        console.log("Player wins: " + playerWins + " -- Computer Wins: " + computerWins);
-        // When 5 rounds have been played, the game will end
-    }
+    // Computer will choose it's play
+    
+    
         // The player with more wins will be declared the winner
     if (playerWins > computerWins) {
         console.log("You win!!!");
@@ -44,60 +42,52 @@ function computerPlay() {
     return choices[num];
 }
 
-// Human will choose their play
-function userPlay() {
-    const userChoices = ['Rock', 'Paper', 'Scissors']
-    let input = '';
-    let pass = false;
-    while(pass === false) {
-            // Player will be prompted to input choice
-        input = prompt("Please enter Rock, Paper, or Scissors");
-            // Player input will be made case insensitive
-        input = input[0].toUpperCase() + input.slice(1).toLowerCase();
-            // Player input will be checked against acceptable inputs
-        if (userChoices.includes(input)) {
-            pass = true;
-        } else {
-            console.log('Please enter a valid input.')
-        }
-    }
-    return input;
-}
+function playRound(e) {
+    
+    const playerSelection = e.target.className;
+    console.log("Player Choice: " + playerSelection)
+    const computerSelection = computerPlay();
 
-function playRound(playerSelection, computerSelection) {
     if (computerSelection === playerSelection) {
-        return "Tie! Play again!";
+        callout.textContent = "Tie! Play again!";
     } else if (playerSelection === "Rock") {
         if (computerSelection === "Paper") {
             round++;
             computerWins++;
-            return "You lose! Paper covers Rock.";
+            callout.textContent = "You lose! Paper covers Rock.";
         }
         else if (computerSelection === "Scissors") {
             round++;
             playerWins++;
-            return "You win!. Rock crushes Scissors.";
+            callout.textContent = "You win!. Rock crushes Scissors.";
         }
     } else if (playerSelection === "Paper") {
         if (computerSelection === "Rock") {
             round++;
             playerWins++;
-            return "You win! Paper covers Rock.";
+            callout.textContent = "You win! Paper covers Rock.";
         }
         else if (computerSelection === "Scissors") {
             round++;
             computerWins++;
-            return "You lose! Scissors cut Paper.";
+            callout.textContent = "You lose! Scissors cut Paper.";
         }
     } else {
         if (computerSelection === "Rock") {
             round++;
             computerWins++;
-            return "You lose! Rock crushes Scissors.";
+            callout.textContent = "You lose! Rock crushes Scissors.";
         } else if (computerSelection === "Paper") {
             round++;
             playerWins++;
-            return "You win! Scissors cut Paper.";
+            callout.textContent = "You win! Scissors cut Paper.";
         }
+    }
+    score.textContent = "Player:" + playerWins + " -- Computer: " + computerWins;
+    round.textContent = "Round " + round;
+    if (playerWins >= 5) {
+        callout.textContent = "You Win!  You have defeated the computer!";
+    } else if (computerWins >= 5) {
+        callout.textContent = "You lose. The computer has beaten you.";
     }
 }
